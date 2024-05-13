a8ab507ac2b5e95d40d7f4748dcb5b94c90b1310
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-05-13 14:42:53 +0200

    Initial commit

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3.14;
+            int b = Convert.toInt32(a);
+            
+            Console.WriteLine("Your number is: " + a);
+            
+            Console.ReadKey();
+        }
+    }
+}
