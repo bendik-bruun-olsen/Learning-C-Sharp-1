66be87c9c2f20b099595ae8c37d31f57fcd7b6f6
Bendik Kodehode <bendik08.kodehode@gmail.com>
2024-05-15 12:52:28 +0200

    hgjhgikj

--- MyFirstProgram/Program.cs
+++ MyFirstProgram/Program.cs
@@ -7,9 +7,13 @@ namespace MyFirstProgram
         static void Main(string[] args)
         {
             double a = 3.14;
-            int b = Convert.toInt32(a);
+            int b = Convert.ToInt32(a);
+            var c = b.GetType();
+            
+            double d = Convert.ToDouble
             
             Console.WriteLine("Your number is: " + a);
+            Console.WriteLine("The datatype of your number is: " + c);
             
             Console.ReadKey();
         }
